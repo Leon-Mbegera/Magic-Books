cc51059812131cdc5b3df9a927fbbe6b83a4ec9c
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-11-07 00:10:55 +0300

    Create initial state with a few starting books

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
 
+const initialState = {
+  books: [
+    {
+      id: Math.floor(Math.random() * 40),
+      title: 'Mediation and Assassination',
+      category: 'History',
+    },
+    {
+      id: Math.floor(Math.random() * 40),
+      title: 'Naked in Death',
+      category: 'Romance',
+    },
+  ],
+};
+
 const rootElement = document.getElementById('root');
 ReactDOM.render(
   <Provider>
