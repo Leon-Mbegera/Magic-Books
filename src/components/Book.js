c93b5cf8a5d1ee1447ee4ed3a683fb5046e7fd87
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-11-06 13:23:38 +0300

    Represent each book and it properties on a single row in a table

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,6 +3,19 @@ import PropTypes from 'prop-types';
 
 const Book = ({ books }) => (
 
-  
+  <tbody>
+    {books.map((bk) => (
+      <tr key={bk.id}>
+        <td>{bk.id}</td>
+        <td>{bk.title}</td>
+        <td>{bk.category}</td>
+      </tr>
+    ))}
+  </tbody>
 );
 
+Book.propTypes = {
+  books: PropTypes.shape.isRequired,
+};
+
+export default Book;
