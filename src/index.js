74cd92ff28404d0c70175eea716c471af1e232d2
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-11-06 11:45:10 +0300

    Attach App component to render its function in main index file

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './components/App';
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  rootElement,
+);
