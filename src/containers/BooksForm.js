c0d232302bd3c6b82d208bf62d9ccdf88e221313
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-11-07 16:53:56 +0300

    Implement handleChange to react to user input, and category selection

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -6,11 +6,20 @@ const BooksForm = () => {
     category: '',
   });
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+
   const Categories = ['Action', 'Romance', 'Mystery', 'History', 'Wildlife', 'Science'];
+  const { title, category } = state;
   return (
     <form className="book-form">
-      <input type="text" placeholder="Add title" autofill="off" />
-      <select>
+      <input type="text" id="title" placeholder="Add title" autofill="off" value={title} onChange={handleChange} />
+      <select id="category">
         {Categories.map((category, idx) => <option key={idx.id}>{category}</option>)}
       </select>
       <button type="submit">Add book</button>
